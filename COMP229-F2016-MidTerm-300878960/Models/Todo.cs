c6ee3d7310f9c34b791d606dee8b8029e682f935
mudrakpatel <mudrakpatel96@gmail.com>
2016-10-19 19:18:58 -0500

    initial commit

--- COMP229-F2016-MidTerm-300878960/Models/Todo.cs
+++ COMP229-F2016-MidTerm-300878960/Models/Todo.cs
@@ -13,7 +13,7 @@ namespace COMP229_F2016_MidTerm_300878960.Models
 
         [Required]
         [StringLength(50)]
-        public string TodoName { get; set; }
+        public string TodoDescription { get; set; }
 
         public string TodoNotes { get; set; }
 
